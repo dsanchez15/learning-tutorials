8cfb65d2566de74055c096e23f4ebd6badca65f0
Duvan <dsanchezr15@gmail.com>
2024-05-28 17:18:22 -0500

    Se agrega el archivo new.

--- netxjs-task-app/src/components/navbar.tsx
+++ netxjs-task-app/src/components/navbar.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link"
 import { ModeToggle } from "./theme-toggle-button"
+import { buttonVariants } from "./ui/button"
 
 function Navbar() {
     return (
         <nav className="flex justify-between">
-            <h1>Titulo Page</h1>
-            <ModeToggle />
+            <h1>NextActionsCRUD</h1>
+            <div className="flex gap-x-2 items-center">
+                <Link href="/new" className={buttonVariants({ variant: "secondary" })}>
+                    Create Task
+                </Link>
+                <ModeToggle />
+            </div>
         </nav>
     )
 }
