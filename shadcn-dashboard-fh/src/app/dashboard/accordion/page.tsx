05e6984b4e069510c0a1d98a8effb30302286102
Duvan <dsanchezr15@gmail.com>
2024-06-03 00:07:34 -0500

    Se hacen los ejecricios con los componentes de Accordion, Alert, Alert Dialog, Badge, Button y Dialog.

--- shadcn-dashboard-fh/src/app/dashboard/accordion/page.tsx
+++ shadcn-dashboard-fh/src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,53 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+    {
+        id: "item-1",
+        question: "Is it accessible?",
+        answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+    },
+    {
+        id: "item-2",
+        question: "Is it styled?",
+        answer: "Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+    },
+    {
+        id: "item-3",
+        question: "Is it animated?",
+        answer: "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+    },
+    {
+        id: "item-4",
+        question: "Can I use it on multiple projects?",
+        answer: "Yes. It's licensed under the MIT license.",
+    },
+    {
+        id: "item-5",
+        question: "Who I am?",
+        answer: "I am Duvan.",
+    },
+];
+
 export default function AccordionPage() {
     return (
         <div>
-            <h1>Accordion Page</h1>
+            <Accordion type="single" collapsible className="w-full">
+                {
+                    /* //Si se cambia el type por multiple y se elimina el collapsible
+                se mantendrian varios */
+                    items.map((item) => (
+                        <AccordionItem value={item.id} key={item.id}>
+                            <AccordionTrigger>{item.question}</AccordionTrigger>
+                            <AccordionContent>{item.answer}</AccordionContent>
+                        </AccordionItem>
+                    ))
+                }
+            </Accordion>
         </div>
     );
 }
